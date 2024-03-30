0539699a7ae2586417c7eb5f6e21ccf2f85d1ff7
Keir <keir@sneezey.uk>
2024-03-30 13:45:07 +0000

    feat: add csproj bits

---
+++ src/test/ExampleTest.gd
@@ -0,0 +1,7 @@
+class_name UserAgreementTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+func test_example() -> void:
+	assert_bool(true)
